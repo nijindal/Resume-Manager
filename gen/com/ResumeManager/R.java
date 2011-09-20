920a80064b1829b54475880e341a82c1476e8a24
Nikhil Jindal <dcenikhi@gmail.com>
2011-09-20 10:16:25 +0530

    commit

--- gen/com/ResumeManager/R.java
+++ gen/com/ResumeManager/R.java
@@ -15,11 +15,14 @@ public final class R {
         public static final int tnp_logo=0x7f020001;
     }
     public static final class id {
-        public static final int Announce=0x7f050007;
+        public static final int Announce_menu=0x7f050005;
+        public static final int Logout_menu=0x7f050007;
         public static final int NavbarLogo=0x7f050001;
-        public static final int announce=0x7f050006;
-        public static final int com_name=0x7f050005;
+        public static final int Recruit_menu=0x7f050006;
+        public static final int announce=0x7f050009;
+        public static final int com_name=0x7f050008;
         public static final int login_button=0x7f050004;
+        public static final int recruit_name=0x7f05000a;
         public static final int rm=0x7f050000;
         public static final int txt_password=0x7f050003;
         public static final int txt_username=0x7f050002;
@@ -27,10 +30,15 @@ public final class R {
     public static final class layout {
         public static final int indexpage=0x7f030000;
         public static final int mainpage=0x7f030001;
-        public static final int rowtext=0x7f030002;
-        public static final int textrow=0x7f030003;
+        public static final int menuoptions=0x7f030002;
+        public static final int recruiters_page=0x7f030003;
+        public static final int rowtext=0x7f030004;
+        public static final int textrow=0x7f030005;
     }
     public static final class string {
+        public static final int Announcements=0x7f040005;
+        public static final int Logout=0x7f040007;
+        public static final int Recruiters=0x7f040006;
         public static final int anmnts=0x7f040002;
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
